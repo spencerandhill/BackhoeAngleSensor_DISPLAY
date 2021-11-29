700c15abe94b357973b73268567ed32215c1982b
spencerandhill <jan.rathmer@gmail.com>
2021-11-29 18:35:27 +0100

    capability added to send data from display to sensor

--- CONSTANTS.ino
+++ CONSTANTS.ino
@@ -11,3 +11,11 @@
 #define BUTTON_PIN 4 // Switch connected with Pin D4
 #define BUTTON_TIME_SET_OFFSET 500
 #define BUTTON_TIME_SWITCH_PAGE 30
+
+
+//#############SECTION ESP NOW #############
+uint8_t broadcastAddressEsp8266Sensor[] = {0xA4, 0xCF, 0x12, 0xBF, 0x76, 0x2D};
+
+//#############SECTION DISPLAY COMMANDS #############
+#define DISPLAY_COMMAND_SET_OFFSET 0
+#define DISPLAY_COMMAND_FLIP_XY 1
--- ESPnow.ino
+++ ESPnow.ino
@@ -2,13 +2,15 @@
 #include <esp_now.h>
 
 // Prototypes
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len);
 
+//###################### RECEIVE DATA ################################
 bool newDataReceived = false;
 
 // Structure to send data
 // Must match the receiver structure
-typedef struct struct_message {
+typedef struct struct_sensor_data {
   float horizonAngle;
   float verticalAngle;
   bool flipXYAxis;
@@ -27,13 +29,36 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
     newDataReceived = true;
 }
 
+
+
+//###################### SEND DATA ################################
+// Structure to receive data
+// Must match the sender structure
+typedef struct struct_sensor_command {
+  int commandType;
+  bool commandValue;
+} struct_sensor_command;
+
+// Create a container for  Sensor Commands from the Display
+struct_sensor_command sensorCommand;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+
+  if(status != ESP_NOW_SEND_SUCCESS) {
+    Serial.println("Delivery Fail");
+  }
+}
+
 void initEspNow() {
   Serial.println("Init ESP-NOW");
   Serial.print("ESP32 Display-Board MAC Address:  ");
-
-  // Set device as a Wi-Fi Station and print MacAddress
-  WiFi.mode(WIFI_MODE_STA);
   Serial.println(WiFi.macAddress());
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
@@ -41,12 +66,38 @@ void initEspNow() {
     return;
   }
 
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, broadcastAddressEsp8266Sensor, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
   // Register Receiver Callback Method and get status of received packet
   esp_now_register_recv_cb(OnDataRecv);
 }
 
-void loopEspNow()
-{
+void sendFlipXYCommand(bool flipXY) {
+
+  sensorCommand.commandType = DISPLAY_COMMAND_FLIP_XY;
+  sensorCommand.commandValue = flipXY;
+
+  // Send message via ESP-NOW
+  int result = esp_now_send(broadcastAddressEsp8266Sensor, (uint8_t *) &sensorCommand, sizeof(sensorCommand));
+  if (result != 0) {
+    Serial.println("Error sending data");
+  }
+
+}
+
+void loopEspNow() {
     if(newDataReceived == true){
         Serial.print("HorizonAngle: ");
         Serial.println(sensorData.horizonAngle);
@@ -55,14 +106,14 @@ void loopEspNow()
 
         Serial.print("FlipXYAxis: ");
         Serial.println(sensorData.flipXYAxis == 1 ? "True" : "False");
-        Serial.println();
 
         Serial.print("Temperature: ");
         Serial.println(sensorData.temperature);
+        Serial.println();
 
         // Now Update the graphics
-    //   updateSliderXY(sensorData.verticalangle, sensordata.horizonangle);
-    //   updateshovelx(sensordata.verticalangle);
+        updateSliderXY(sensorData.verticalAngle, sensorData.horizonAngle);
+        updateShovelX(sensorData.verticalAngle);
 
         // Reset for the next loop-run
         newDataReceived = false;
